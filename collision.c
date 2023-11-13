c2e5175e39f9ec241b9f38e16ec8613c7af6a30e
Tomyung1 <112257349+Tomyung1@users.noreply.github.com>
2023-11-13 14:51:26 +0100

    Add files via upload

--- collision.c
+++ collision.c
@@ -10,5 +10,30 @@ int collision_rectangles(vect* hitbox1, vect* pos1, vect* hitbox2, vect* pos2){
 	  (hitbox1 -> x + hitbox1 -> x >= hitbox2 -> y) &&
 	  (hitbox1 -> y <= hitbox2 -> y + hitbox2 -> y) &&
 	  (hitbox1 -> y + hitbox1 -> y >= hitbox2 -> y));
-  /* collision si :   à gauche           et                à droite        et             en haut            et             en bas */
+}
+
+void resolution_collision(game* game){
+    int jou, enn, bal;
+    vect *jou_hit, *jou_pos, *enn_hit, *enn_pos;
+    
+    for (jou = 0; jou < game -> n_joueurs; jou++){
+
+        jou_hit = &(game -> joueurs[jou].hitbox);
+        jou_pos = &(game -> joueurs[jou].pos);
+        
+        for (enn = 0; enn < game -> n_ennemis; enn++){
+
+            enn_hit = &(game -> ennemis[enn].hitbox);
+            enn_pos = &(game -> ennemis[enn].pos);
+
+            /* collisions joueurs <-> ennemis */
+            if (collision_rectangles(jou_hit, jou_pos, enn_hit, enn_pos)){
+                game -> joueurs[jou].vie -= DAMAGE_COLLISION;
+
+                if (!(game -> joueurs[jou].vie)){
+                    
+                }
+            }
+        }
+    }
 }
