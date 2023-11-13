5caf4c5298224078f88a52dd8fd74a7762a1b9b9
Tomyung1 <112257349+Tomyung1@users.noreply.github.com>
2023-11-13 14:52:28 +0100

    Add files via upload
    Ajout fonction principale de collision.c

--- headers/collision.h
+++ headers/collision.h
@@ -1,6 +1,10 @@
 #ifndef _COLLISION_H_
 #define _COLLISION_H_
 
+#define DAMAGE_COLLISION 2
+
 int collision_rectangles(vect* hitbox1, vect* pos1, vect* hitbox2, vect* pos2);
+
+void resolution_collision(game* game);
 
 #endif /* _COLLISION_H_ */
