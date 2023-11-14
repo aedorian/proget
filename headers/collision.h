9b90db7186ccf40152e43cfe3c3968cd94698926
Tomyung1 <112257349+Tomyung1@users.noreply.github.com>
2023-11-14 17:00:21 +0100

    Add collision.h
    Ajout des fonctions de collision.c et macro DAMAGE_COLLISION

--- headers/collision.h
+++ headers/collision.h
@@ -5,6 +5,8 @@
 
 int collision_rectangles(vect* hitbox1, vect* pos1, vect* hitbox2, vect* pos2);
 
+void reduction_tableau(game *game, int est_balle_sinon_ennemi);
+
 void resolution_collision(game* game);
 
 #endif /* _COLLISION_H_ */
